a42867624b65bc7f67b3e4063d63c3e6d68dea63
Shashank Kharel <shashank.kharel@hms.com>
2019-12-02 13:35:28 -0600

    initializing repository

---
+++ app_start.ps1
@@ -0,0 +1,38 @@
+﻿# >>> read-host -assecurestring | convertfrom-securestring | out-file U:\mysecurestring.txt
+# Start-Process -FilePath 'C:\Program Files (x86)\Mozilla Firefox\firefox.exe' -Verb runAs -Credntial
+# $userName = "HMS\e006487"
+# $password = "JunkPassWORD"
+
+# function New-Credential($u,$p) {
+#   $secpasswd = ConvertTo-SecureString $p -AsPlainText -Force
+#    return New-Object System.Management.Automation.PSCredential ($u, $secpasswd)
+# }
+# $cred = New-Credential $userName $password
+# Start-Process -FilePath 'C:\Program Files (x86)\Mozilla Firefox\firefox.exe' -Credential $cred
+# Write-Output $ 
+
+# process called wscript
+
+
+
+$users = Get-Content -Path U:\userlist.txt
+foreach($username in $users)
+{
+    $password = Get-Content "c:\Users\$user\Documents\mysecurestring.txt" | ConvertTo-SecureString
+    $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $password
+    Stop-Process -Name "wscript" -Credential $cred
+    }
+
+#Insert DB2 script -pre
+#Insert copy of files for every user
+
+Start-Sleep -Seconds 60
+
+foreach($username in $users)
+{
+    $password = Get-Content "c:\Users\$user\Documents\mysecurestring.txt" | ConvertTo-SecureString
+    $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $password
+    Start-Process -Name "wscript" -Credential $cred
+    }
+
+#Insert DB2 script -post
---
+++ for_loop_example.ps1
@@ -0,0 +1,6 @@
+﻿# Invoke-Command –ComputerName hmsit01 –ScriptBlock {Start-Process notepad.exe} -Credential
+# $cred = Get-Credential
+# Invoke-WmiMethod –ComputerName hmsit01.hms.hmsy.com -Class win32_process -Name create -ArgumentList "notepad.exe"
+for ($i=0; $i -le 3; $i++ ) { 
+    Start-Process  "C:\Users\e006487\Desktop\putty.exe"
+    }
